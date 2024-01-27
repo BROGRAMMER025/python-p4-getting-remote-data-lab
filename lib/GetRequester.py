05464633d439dd50193299b7963349c8e3fe146a
allan murigi <allan.murigi@student.moringaschool.com>
2024-01-28 01:22:41 +0300

    complete:remote data lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,3 +1,4 @@
+
 import requests
 import json
 
@@ -7,7 +8,35 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            response = requests.get(self.url)
+            
+            if response.status_code == 200:
+                return response.text
+            else:
+                return f"Error: {response.status_code}"
+        except requests.RequestException as e:
+            return f"Error: {e}"
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            
+            json_data = json.loads(response_body)
+            return json_data
+        except json.JSONDecodeError as e:
+            return f"Error decoding JSON: {e}"
+
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+requester = GetRequester(url)
+
+
+response_body = requester.get_response_body()
+print("Response Body:")
+print(response_body)
+
+
+json_data = requester.load_json()
+print("\nJSON Data:")
+print(json_data)
